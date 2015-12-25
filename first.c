e4b4a2280c41dbfb5a80f16b5c38111c64078322
Eli Tabello <elit@exposoft.ca>
2015-12-25 15:09:11 -0500

    button wired and works

--- first.c
+++ first.c
@@ -14,27 +14,25 @@ static void test_btn_click (GtkWidget *widget, gpointer *data)
 
 int main( int  argc, char *argv[] )  
 {  
-    char *title;
-    strcpy(title, "Title");   
-
     GtkWidget *window;  
     GtkWidget *button;
 
     gtk_init (&argc, &argv);  
 
     window = gtk_window_new (GTK_WINDOW_TOPLEVEL);  
-    gtk_window_set_title (GTK_WINDOW (window), title);
+    gtk_window_set_title (GTK_WINDOW (window), "Title");
+    gtk_container_set_border_width (GTK_CONTAINER (window), 10);
 
     // create button
-    //button = gtk_button_new_with_label("Test: Click Me");
+    button = gtk_button_new_with_label("Test: Click Me");
     
     // wire up button
-    //g_signal_connect(button, "clicked", G_CALLBACK(test_btn_click), NULL);
+    g_signal_connect(button, "clicked", G_CALLBACK(test_btn_click), NULL);
 
     // add button to window
-    //gtk_container_add(GTK_CONTAINER (window), button);
+    gtk_container_add(GTK_CONTAINER (window), button);
 
-    //gtk_widget_show(button);
+    gtk_widget_show(button);
     gtk_widget_show(window);  
     gtk_main ();  
     return 0;  
