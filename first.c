d66b56401233658ba74944f9d0431994e814bef4
Eli Tabello <elit@exposoft.ca>
2015-12-25 15:06:09 -0500

    button

--- first.c
+++ first.c
@@ -6,22 +6,36 @@
 // if no argument is sent, "Title" is the title
 
  
+// on click event
+static void test_btn_click (GtkWidget *widget, gpointer *data)
+{
+    g_print("Test Click Event\n");
+}
+
 int main( int  argc, char *argv[] )  
 {  
     char *title;
-    if (argc == 0)
-    {
-        strcpy(title, "Title");   
-    }
-    else
-    {
-        strcpy(title, argv[1]);
-    }
+    strcpy(title, "Title");   
+
     GtkWidget *window;  
+    GtkWidget *button;
+
     gtk_init (&argc, &argv);  
+
     window = gtk_window_new (GTK_WINDOW_TOPLEVEL);  
     gtk_window_set_title (GTK_WINDOW (window), title);
-    gtk_widget_show (window);  
+
+    // create button
+    //button = gtk_button_new_with_label("Test: Click Me");
+    
+    // wire up button
+    //g_signal_connect(button, "clicked", G_CALLBACK(test_btn_click), NULL);
+
+    // add button to window
+    //gtk_container_add(GTK_CONTAINER (window), button);
+
+    //gtk_widget_show(button);
+    gtk_widget_show(window);  
     gtk_main ();  
     return 0;  
  }  
